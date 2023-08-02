8912b62ad4a9a2e678fd950fe0a7d80f3be522c1
Buse Latife BEKER <busebeker.bb@gmail.com>
2023-08-02 09:57:15 +0300

    javascript

---
+++ js/tutorials.js
@@ -0,0 +1,13 @@
+//Single Comment
+/*
+Multiple Comment
+*/
+
+console.log("loglama");
+console.info("info");
+console.warn("warn");
+console.error("error");
+
+alert("pop-up bilgilendirme-1");
+window.alert("pop-up bilgilendirme-2");
+window.document.writeln("ekranda yazı");
