a930f1923e87aef36be69fd1721b952852592db9
Buse Latife BEKER <busebeker.bb@gmail.com>
2023-08-02 11:33:53 +0300

    whoisting, variable, console, alert, window

--- js/tutorials.js
+++ js/tutorials.js
@@ -1,13 +1,56 @@
+// JS 1995 yılında Brandan Eich tarafından yazılıyor.
+// Cross platform
+//OOP uygun
+
 //Single Comment
 /*
 Multiple Comment
 */
 
-console.log("loglama");
-console.info("info");
-console.warn("warn");
-console.error("error");
+// console.log("loglama");
+// console.info("info");
+// console.warn("warn");
+// console.error("error");
 
-alert("pop-up bilgilendirme-1");
-window.alert("pop-up bilgilendirme-2");
-window.document.writeln("ekranda yazı");
+// alert("pop-up bilgilendirme-1");
+// window.alert("pop-up bilgilendirme-2");
+// window.document.writeln("ekranda yazı");
+
+// Variable
+var result = 4423;
+console.log(typeof result+" => "+result);
+
+var result = 44.23;
+console.log(typeof result+" => "+result)
+
+var result = "Js öğreniyorum";
+console.log(typeof result+" => "+result)
+
+var result = true;
+console.log(typeof result+" => "+result)
+
+// Naming Convensition
+var deneme="JS Data"
+console.log(deneme);
+
+var deneme44="JS Data"
+console.log(deneme44);
+
+var $deneme44="JS Data"
+console.log($deneme44);
+
+var _$deneme44="JS Data"
+console.log(_$deneme44);
+
+var denemeVerisi="JS Data"
+console.log(denemeVerisi);
+
+// var ~44$deneme44="JS Data"
+// console.log(~44$deneme44);
+
+// whoisting
+result2=56;
+var result2;
+
+/////////////////////////////////////////////////////////
+// Operators
